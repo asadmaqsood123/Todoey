98cbb3854ad6556bb2f498a700f433a10b4d799a
Asad Maqsood <mughalasad526@gmail.com>
2018-07-27 19:26:55 +0500

    first commit

---
+++ Todoey/TodoListController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Asad asadmaqsood on 26/07/2018.
+//  Copyright © 2018 Asad Maqsood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
